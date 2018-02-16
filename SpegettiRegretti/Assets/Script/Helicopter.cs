7eff2941b432edf0095bd4fb688677923fdb36fa
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2018-02-16 13:37:28 +0100

    Unity Project hotdog follow
    Hotdog truck follows and you run and death and songs and yes.

---
+++ SpegettiRegretti/Assets/Script/Helicopter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x + 10f, transform.position.y + .1f, transform.position.z + 30f);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 100f, transform.eulerAngles.z);
+ 
+    }
+}
